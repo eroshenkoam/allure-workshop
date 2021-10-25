98d61362adfacc28cbd3e9d1acfbba7a3f5625c5
Artem Eroshenko <eroshenkoam@qameta.io>
2021-10-25 15:54:15 +0300

    automation for 37973

--- src/test/java/io/qameta/allure/AuthTest.java
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -1,65 +1,25 @@
 package io.qameta.allure;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static io.qameta.allure.Allure.step;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Owner("admin")
 @Feature("Auth")
-@Microservice("Auth")
 @Story("External Auth")
 public class AuthTest {
 
     @Test
-    @AllureId("37948")
-    @JiraIssue("AE-1")
-    @Tags({@Tag("web"), @Tag("smoke")})
-    @DisplayName("Авторизация через Facebook")
-    public void testFacebook() {
+    @AllureId("37973")
+    @DisplayName("Авторизация через Google")
+    public void testGoogle() {
         step("Открываем главную страницу");
-        step("Выбираем авторизацию через Facebook", () -> {
-            step("Нажимаем кнопку Войти");
-            step("Нажимаем кнопку Facebook");
-            step("Проверяем что перешли на страницу Facebook");
-        });
-        step("Авторизуемся как пользователь 'Mr Random'", () -> {
-            step("Вводим логин 'random-user@gmail.com'");
-            step("Вводим пароль 'random-pass'");
-        });
-        step("Проверяем что успешно авторизовались", () -> {
-            step("Текущая страница - это глаавня страницу приложения");
-            step("Логин пользователя 'test-ramdom'");
+        step("Авторизуемся как пользователь eroshenkoam");
+        step("Проверяем что авторизовались верно", () -> {
+            step("Редирект на главную страницу сервиса");
+            step("Логин eroshnekoam ");
+            step("Имся Artem Eroshenko");
         });
     }
-
-    @Test
-    @JiraIssue("AE-1")
-    @AllureId("37949")
-    @Tags({@Tag("web"), @Tag("smoke")})
-    @DisplayName("Авторизация через GitHub")
-    public void testGitHub() {
-        step("Открываем главную страницу");
-        step("Выбираем авторизацию через GitHub", () -> {
-            step("Нажимаем кнопку Войти");
-            step("Нажимаем кнопку GitHub");
-            step("Проверяем что перешли на страницу GitHub");
-        });
-        step("Авторизуемся как пользователь 'Mr Test'", () -> {
-            step("Вводим логин 'test-user@gmail.com'");
-            step("Вводим пароль 'test-pass'");
-            step("Нажимаем кнопку Войти");
-        });
-        step("Проверяем что успешно авторизовались", () -> {
-            step("Текущая страница - это глаавня страницу приложения");
-            step("Имя пользователя 'Mr Test'");
-            step("Логин пользователя 'test-user'");
-        });
-    }
-
 }
