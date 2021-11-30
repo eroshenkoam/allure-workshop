788484eddfc9ee62ae6f27f73d026959ebe26ece
Artem Eroshenko <eroshenkoam@qameta.io>
2021-11-30 20:39:52 +0300

    add test 88708

---
+++ src/test/java/io/qameta/allure/IssuesTest.java
@@ -0,0 +1,33 @@
+package io.qameta.allure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Feature("Задачи")
+@Owner("eroshenkoam")
+public class IssuesTest {
+
+    private final String REPOSITORY = "owner/repo";
+    private final int ISSUE = 74;
+
+    @Test
+    @Tag("critical")
+    @AllureId("88708")
+    @Story("Поиск задачи в репозитории")
+    @DisplayName("Неавторизованный пользователь должен иметь возможность найти задачу в репозитории")
+    public void testIssueSearch() {
+        step("Открывает главную страницу");
+        step("Ищем репозиторий " + REPOSITORY + " в форме поиска в шапке", () -> {
+            step("Кликаем на поисковую строку");
+            step("Вводим текст " + REPOSITORY);
+            step("Нажимаем кнопку Enter");
+        });
+        step("Переходим по ссылке репозитория " + REPOSITORY);
+        step("Переходим в Tab Задачи");
+        step("Проверяем что Задача с номером #" + ISSUE + " существует");
+    }
+
+}
